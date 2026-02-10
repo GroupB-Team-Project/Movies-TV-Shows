9038dd60dcc713a4e620007616ff0969e689df4f
AaronBridgerNci <x24247421@student.ncirl.ie>
2026-02-10 16:47:21 +0000

    Updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const TMDB_API_KEY = "a3585bbddb78faddcc9969920abce760";
-const moviesContainer = document.getElementById("movies");
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
