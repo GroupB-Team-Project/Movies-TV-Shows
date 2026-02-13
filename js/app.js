9d978ad2bb173d3d67bc25c6786b8c1d071485a8
SarahP-05 <sarahpettigrew31@gmail.com>
2026-02-13 15:55:19 +0000

    Updating Light/Dark Modes

--- js/app.js
+++ js/app.js
@@ -103,3 +103,26 @@ tvTab.onclick = () => {
 // START APP
 // =======================
 fetchMovies();
+
+
+//LIGHT MODE TOGGLE
+
+   const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+      toggleBtn.textContent = "🌙 Dark Mode";
+      localStorage.setItem("theme", "light");
+    } else {
+      toggleBtn.textContent = "☀️ Light Mode";
+      localStorage.setItem("theme", "dark");
+    }
+  });
+
+  // Load saved preference
+  if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
